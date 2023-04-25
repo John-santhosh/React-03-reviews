5df777b11f51df1a8b47e04f35d6b2e8da3125cc
john <johnsantosh2@gmail.com>
2023-04-25 13:50:06 +0530

    completed build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,69 @@
+import { useState } from "react";
+import data from "./data";
+import { FaAngleLeft, FaAngleRight, FaQuoteRight } from "react-icons/fa";
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+
+  const checkNumber = (number) => {
+    if (number >= data.length) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+  function nextBtn() {
+    setIndex((currIndex) => {
+      const newIndex = currIndex + 1;
+      return checkNumber(newIndex);
+    });
+  }
+
+  function prevBtn() {
+    setIndex((currIndex) => {
+      const newIndex = currIndex - 1;
+      return checkNumber(newIndex);
+    });
+  }
+
+  function surprise() {
+    let random = Math.floor(Math.random() * data.length);
+    if (random === index) {
+      random += 1;
+    }
+    setIndex(checkNumber(random));
+  }
+
+  const { name, image, text, job } = data[index];
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevBtn}>
+            {<FaAngleLeft />}
+          </button>
+          <button className="next-btn" onClick={nextBtn}>
+            {<FaAngleRight />}
+          </button>
+        </div>
+
+        <button className="btn" onClick={surprise}>
+          Surprise Me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
